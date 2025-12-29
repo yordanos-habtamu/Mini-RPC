db5d7671c38ef6024477a33f767f27cb026c5d83
yordanos habtamu <jordrichh7@gmail.com>
2025-12-29 15:30:57 +0300

    update the protocol to make bidirectional stream

--- rpc/protocol.go
+++ rpc/protocol.go
@@ -1,17 +1,26 @@
 package rpc
 
 type RPCType int
-
-
 const (
 	Unary RPCType = iota
 	Streaming
+	Bidirectional
 )
+type MessageType string
+
+const (
+	MsgCall MessageType ="call"
+	MsgStream MessageType ="stream"
+)
+
+
 
 type Request struct {
 	ID uint64 `json:"id"`
-	Method string `json:"method"`
-	Params map[string]any `json:"params"`
+	Type MessageType `json:"type"`
+	Method string `json:"method,omitempty"`
+	Params map[string]any `json:"params,omitempty"`
+	Data any `json:"data,omitempty"`
 }
 type Cancel struct{
 	ID uint64  `json:"id"`
@@ -20,4 +29,9 @@ type Response struct {
 	ID uint64 `json:"id"`
 	Result any `json:"result"`
 	Error  string `json:"error,omitempty"`
+}
+
+type Stream struct {
+	In <- chan any
+	Out chan<- any
 }
